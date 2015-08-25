24d144c0da2cb4b051ab4dba971170cf0bf1094b
potmadu <potmadu@gmail.com>
2015-08-25 14:22:49 -0700

    Integrate Video Panel

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +11,15 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using AvalonDock;
+using System.Windows.Controls;
+using System.Windows.Forms;
+using Declarations;
+using Declarations.Events;
+using Declarations.Media;
+using Declarations.Players;
+using Implementation;
+using System.ComponentModel;
+using System.Windows.Controls.Primitives;
 
 namespace Sample3
 {
@@ -20,22 +28,43 @@ namespace Sample3
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        IMediaPlayerFactory m_factory;
+        IVideoPlayer m_player;
+        IMediaFromFile m_media;
+        private volatile bool m_isDrag;
+
         public MainWindow()
         {
             InitializeComponent();
             BuildDockingLayout();
             this.WindowState = System.Windows.WindowState.Maximized;
+            System.Windows.Application.Current.Resources["ThemeDictionary"] = new ResourceDictionary();
 
+            System.Windows.Forms.Panel p = new System.Windows.Forms.Panel();
+            p.BackColor = System.Drawing.Color.Black;
+            windowsFormsHost1.Child = p;
 
-            Application.Current.Resources["ThemeDictionary"] = new ResourceDictionary();
+            m_factory = new MediaPlayerFactory(true);
+            m_player = m_factory.CreatePlayer<IVideoPlayer>();
+
+            this.DataContext = m_player;
+
+            m_player.Events.PlayerPositionChanged += new EventHandler<MediaPlayerPositionChanged>(Events_PlayerPositionChanged);
+            m_player.Events.TimeChanged += new EventHandler<MediaPlayerTimeChanged>(Events_TimeChanged);
+            m_player.Events.MediaEnded += new EventHandler(Events_MediaEnded);
+            m_player.Events.PlayerStopped += new EventHandler(Events_PlayerStopped);
+
+            m_player.WindowHandle = p.Handle;
+            slider2.Value = m_player.Volume;
         }
 
         void BuildDockingLayout()
         {
-            dockManager.Content = null;
+            //dockManager.Content = null;
 
             //TreeView dockable content
-            var trv = new TreeView();
+            var trv = new System.Windows.Controls.TreeView();
             trv.Items.Add(new TreeViewItem() { Header = "Item1" });
             trv.Items.Add(new TreeViewItem() { Header = "Item2" });
             trv.Items.Add(new TreeViewItem() { Header = "Item3" });
@@ -46,34 +75,19 @@ namespace Sample3
             ((TreeViewItem)trv.Items[2]).Items.Add(new TreeViewItem() { Header = "SubItem4" });
             var treeviewContent = new DockableContent() { Title = "Explorer", Content = trv };
 
+            //Left TreeView dockable content
+            var treeviewContent_left = new DockableContent() { Title = "Explorer Info Left", Content = new System.Windows.Controls.TextBox() { Text = "Explorer Info Text", IsReadOnly = true } };
+            treeviewContent_left.Show(dockManager, AnchorStyle.Left);
+            treeviewContent_left.ToggleAutoHide();
+
+            //Left TreeView dockable content
+            var treeviewContent_right = new DockableContent() { Title = "Explorer Info Right", Content = new System.Windows.Controls.TextBox() { Text = "Explorer Info Text", IsReadOnly = true }, FlyoutWindowSize = new Size(500, 500) };
+            treeviewContent_right.Show(dockManager, AnchorStyle.Right);
             treeviewContent.Show(dockManager, AnchorStyle.Bottom);
 
             //TextBox invo dockable content
-            var treeviewInfoContent = new DockableContent() { Title = "Explorer Info", Content = new TextBox() { Text = "Explorer Info Text", IsReadOnly = true } };
+            var treeviewInfoContent = new DockableContent() { Title = "Explorer Info", Content = new System.Windows.Controls.TextBox() { Text = "Explorer Info Text", IsReadOnly = true } };
             treeviewContent.ContainerPane.Items.Add(treeviewInfoContent);
-
-            //ListView dockable content
-            var gridView = new GridView();
-            gridView.Columns.Add(new GridViewColumn() { Header = "Date" });
-            gridView.Columns.Add(new GridViewColumn() { Header = "Day Of Weeek", DisplayMemberBinding = new Binding("DayOfWeek") });
-            gridView.Columns.Add(new GridViewColumn() { Header = "Year", DisplayMemberBinding = new Binding("Year") });
-            gridView.Columns.Add(new GridViewColumn() { Header = "Month", DisplayMemberBinding = new Binding("Month") });
-            gridView.Columns.Add(new GridViewColumn() { Header = "Second", DisplayMemberBinding = new Binding("Second") });
-            var listView = new ListView() { View = gridView };
-            listView.Items.Add(DateTime.Now);
-            listView.Items.Add(DateTime.Now.AddYears(-1));
-            listView.Items.Add(DateTime.Now.AddMonths(15));
-            listView.Items.Add(DateTime.Now.AddHours(354));
-
-            //Left TreeView dockable content
-            var treeviewContent_left = new DockableContent() { Title = "Explorer Info Left", Content = new TextBox() { Text = "Explorer Info Text", IsReadOnly = true } };
-
-            treeviewContent_left.Show(dockManager, AnchorStyle.Left);
-
-            //Left TreeView dockable content
-            var treeviewContent_right = new DockableContent() { Title = "Explorer Info Right", Content = new TextBox() { Text = "Explorer Info Text", IsReadOnly = true } };
-
-            treeviewContent_right.Show(dockManager, AnchorStyle.Right);
 
         }
 
@@ -84,20 +98,138 @@ namespace Sample3
 
         private void ChangeCustomTheme(object sender, RoutedEventArgs e)
         {
-            string uri = (string)((MenuItem)sender).Tag;
+            string uri = (string)((System.Windows.Controls.MenuItem)sender).Tag;
             ThemeFactory.ChangeTheme(new Uri(uri, UriKind.RelativeOrAbsolute));
         }
 
         private void ChangeStandardTheme(object sender, RoutedEventArgs e)
         {
-            string name = (string)((MenuItem)sender).Tag;
+            string name = (string)((System.Windows.Controls.MenuItem)sender).Tag;
             ThemeFactory.ChangeTheme(name);
         }
 
         private void ChangeColor(object sender, RoutedEventArgs e)
         {
-            
-            ThemeFactory.ChangeColors((Color)ColorConverter.ConvertFromString(((MenuItem)sender).Header.ToString()));
+
+            ThemeFactory.ChangeColors((Color)ColorConverter.ConvertFromString(((System.Windows.Controls.MenuItem)sender).Header.ToString()));
+        }
+
+        void Events_PlayerStopped(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                InitControls();
+            }));
+        }
+
+        void Events_MediaEnded(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                InitControls();
+            }));
+        }
+
+        private void InitControls()
+        {
+            slider1.Value = 0;
+            label1.Content = "00:00:00";
+            label3.Content = "00:00:00";
+        }
+
+        void Events_TimeChanged(object sender, MediaPlayerTimeChanged e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                label1.Content = TimeSpan.FromMilliseconds(e.NewTime).ToString().Substring(0, 8);
+            }));
+        }
+
+        void Events_PlayerPositionChanged(object sender, MediaPlayerPositionChanged e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                if (!m_isDrag)
+                {
+                    slider1.Value = (double)e.NewPosition;
+                }
+            }));
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                textBlock1.Text = ofd.FileName;
+                m_media = m_factory.CreateMedia<IMediaFromFile>(ofd.FileName);
+                m_media.Events.DurationChanged += new EventHandler<MediaDurationChange>(Events_DurationChanged);
+                m_media.Events.StateChanged += new EventHandler<MediaStateChange>(Events_StateChanged);
+
+                m_player.Open(m_media);
+                m_media.Parse(true);
+            }
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.Play();
+        }
+
+        void Events_StateChanged(object sender, MediaStateChange e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+
+            }));
+        }
+
+        void Events_DurationChanged(object sender, MediaDurationChange e)
+        {
+            this.Dispatcher.BeginInvoke(new Action(delegate
+            {
+                label3.Content = TimeSpan.FromMilliseconds(e.NewDuration).ToString().Substring(0, 8);
+            }));
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.Pause();
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.Stop();
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            m_player.ToggleMute();
+        }
+
+        private void slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (m_player != null)
+            {
+                m_player.Volume = (int)e.NewValue;
+            }
+        }
+
+        private void slider1_DragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            m_player.Position = (float)slider1.Value;
+            m_isDrag = false;
+        }
+
+        private void slider1_DragStarted(object sender, DragStartedEventArgs e)
+        {
+            m_isDrag = true;
+        }
+
+        private void buttonSnapshot_Click_1(object sender, RoutedEventArgs e)
+        {
+            m_player.Pause();
+            m_player.TakeSnapShot(0, @"c:\temp\");
         }
     }
 }
